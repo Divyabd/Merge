b87d3b9539999f55975debd0f411a3d564165f19
Divyabd <divyagowda8393@gmail.com>
2021-02-08 12:28:52 +0530

    mege commit

---
+++ Merge/src/merge/Merge.java
@@ -0,0 +1,12 @@
+package merge;
+
+public class Merge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		System.out.println(a+b);
+	}
+
+}
